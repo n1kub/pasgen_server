e741728b082354c85fabab9e8a5b9f5b0e018cab
n1kub <n1kub@mail.ru>
2023-02-24 10:04:33 +0300

    added function that modifies old password

--- service/password_generation.py
+++ service/password_generation.py
@@ -18,4 +18,21 @@ def generate_new_password(length: int, has_uppercase: bool, has_special_symbols:
 
 def update_password(old_password: str, add_uppercase: bool, add_special_symbols: bool) -> str:
     new_pass = old_password.lower()
+    special_characters = string.digits + string.punctuation
+    if add_uppercase and add_special_symbols:
+        for char in old_password:
+            new_pass.join(char.upper() if bool(random.getrandbits(1)) else char)
+            new_pass.join(random.choice(special_characters) if bool(random.getrandbits(1)) else "")
+        return new_pass
+    elif add_special_symbols:
+        for _ in old_password:
+            new_pass.join(random.choice(special_characters) if bool(random.getrandbits(1)) else "")
+        return new_pass
+    elif add_uppercase:
+        for char in old_password:
+            new_pass.join(char.upper() if bool(random.getrandbits(1)) else char)
+        return new_pass
+    else:
+        return new_pass
+
     
